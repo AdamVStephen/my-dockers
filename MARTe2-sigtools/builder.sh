49dc6f821d6a9cc41d06e0ca15f2d88cc45c5873
Adam V Stephen (ukaea-fpga) <adam.v.stephen@gmail.com>
2022-01-30 19:33:31 +0000

    Tests for debian and ubuntu from fpga

--- MARTe2-sigtools/builder.sh
+++ MARTe2-sigtools/builder.sh
@@ -10,7 +10,9 @@ buildit() {
 #time docker build -t avstephen/marte2-sigtools-centos7:codename -f Dockerfile.centos7 . 2>&1 | tee build.sigtools.centos7.codename.N
 #time docker build -t avstephen/marte2-sigtools-ubuntu-1804:airdrie -f Dockerfile.ubuntu-18.04 . 2>&1 | tee build.sigtools.ubuntu-18.04.airdrie.1
 #time docker build -t avstephen/marte2-sigtools-ubuntu-1804:airdrie -f Dockerfile.ubuntu-18.04 . 2>&1 | tee build.sigtools.ubuntu-18.04.airdrie.1
-time docker build -t avstephen/marte2-sigtools-ubuntu-1804:airdrie -f Dockerfile.ubuntu-18.04 . 2>&1 | tee build.sigtools.ubuntu-18.04.airdrie.2
+#time docker build -t avstephen/marte2-sigtools-ubuntu-1804:airdrie -f Dockerfile.ubuntu-18.04 . 2>&1 | tee build.sigtools.ubuntu-18.04.airdrie.2
+time docker build -t avstephen/marte2-sigtools-ubuntu-1804:arbroath -f Dockerfile.ubuntu-18.04.minimised . 2>&1 | tee build.sigtools.ubuntu-18.04.arbroath.1
+time docker build -t avstephen/marte2-sigtools-debian-11:arbroath -f Dockerfile.debian.minimised . 2>&1 | tee build.sigtools.debian.arbroath.1
 }
 
 build_tests() {
