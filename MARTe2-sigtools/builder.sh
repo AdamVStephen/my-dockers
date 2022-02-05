0d0fdd8f00a397461afedd1be030551fba430edb
Adam V Stephen(adam@jump) <adam.v.stephen@gmail.com>
2022-02-05 17:56:48 +0000

    Fully refactored build script.

--- MARTe2-sigtools/builder.sh
+++ MARTe2-sigtools/builder.sh
@@ -2,8 +2,10 @@
 #
 # Latch the build commands to ensure reproducibility.
 
+# TODO: test all combinations (cache/no-cache)
+
 usage() {
-  echo "usage"
+	echo "usage"
 }
 
 ###
@@ -16,30 +18,30 @@ export DOCKER_TAG_CENTOS7=avstephen/marte2-sigtools-centos7:${CODENAME_CENTOS7}
 
 build_centos7 () {
 
-  TARGET=$1
-  do_cache=$2
+	TARGET=$1
+	invalidate_cache=$2
 
-  if [ x"$do_cache" == "xy" ]
-  then
-    CACHE_OPT="--build-arg CACHE_DATE=$(date +%s) "
-  fi
+	if [ x"$invalidate_cache" == "xy" ]
+	then
+		CACHE_OPT="--build-arg CACHE_DATE=$(date +%s) "
+	fi
 
-  this_log="build.centos7.${TARGET}.${CODENAME_CENTOS7}.$(date +%s)"
+	this_log="build.centos7.${TARGET}.${CODENAME_CENTOS7}.$(date +%s)"
 
-  time docker build "${CACHE_OPT:=' '}" \
-    --target "${TARGET}" \
-    -t "${DOCKER_TAG_CENTOS7}" \
-    -f "${DOCKERFILE_CENTOS7}" . 2>&1 | tee "$this_log"
+	time docker build "${CACHE_OPT}" \
+		--target "${TARGET}" \
+		-t "${DOCKER_TAG_CENTOS7}" \
+		-f "${DOCKERFILE_CENTOS7}" . 2>&1 | tee "$this_log"
 
-  ln -fs "$this_log" centos7.last.log
-    
+	ln -fs "$this_log" centos7.last.log
+
 }
 
 centos7() {
-build_centos7 m2st_base n
-build_centos7 m2st_packages n
-build_centos7 m2st_dependencies n
-build_centos7 m2st_built n
+	build_centos7 m2st_base n
+	build_centos7 m2st_packages n
+	build_centos7 m2st_dependencies n
+	build_centos7 m2st_built n
 }
 
 ###
@@ -52,30 +54,33 @@ export DOCKER_TAG_UBUNTU1804=avstephen/marte2-sigtools-ubuntu1804:${CODENAME_UBU
 
 build_ubuntu1804 () {
 
-  TARGET=$1
-  do_cache=$2
+	TARGET=$1
+	invalidate_cache=$2
 
-  if [ x"$do_cache" == "xy" ]
-  then
-    CACHE_OPT="--build-arg CACHE_DATE=$(date +%s) "
-  fi
+	if [ x"$invalidate_cache" == "xy" ]
+	then
+		CACHE_OPT="--build-arg CACHE_DATE=$(date +%s) "
+	else
+		unset CACHE_OPT
+	fi
 
-  this_log="build.ubuntu1804.${TARGET}.${CODENAME_UBUNTU1804}.$(date +%s)"
+	this_log="build.ubuntu1804.${TARGET}.${CODENAME_UBUNTU1804}.$(date +%s)"
 
-  time docker build "${CACHE_OPT:=' '}" \
-    --target "${TARGET}" \
-    -t "${DOCKER_TAG_UBUNTU1804}" \
-    -f "${DOCKERFILE_UBUNTU1804}" . 2>&1 | tee "$this_log"
+	# shellcheck exemption: quotes around ${CACHE_OPT} would be harmful
+	time docker build ${CACHE_OPT} \
+		--target "${TARGET}" \
+		-t "${DOCKER_TAG_UBUNTU1804}" \
+		-f "${DOCKERFILE_UBUNTU1804}" . 2>&1 | tee "$this_log"
 
-  ln -fs "$this_log" ubuntu1804.last.log
-    
+	ln -fs "$this_log" ubuntu1804.last.log
+
 }
 
 ubuntu1804() {
-build_ubuntu1804 m2st_base n
-build_ubuntu1804 m2st_packages n
-build_ubuntu1804 m2st_dependencies n
-build_ubuntu1804 m2st_built n
+	#build_ubuntu1804 m2st_base n
+	#build_ubuntu1804 m2st_packages n
+	#build_ubuntu1804 m2st_dependencies n
+	build_ubuntu1804 m2st_built n
 }
 
 ###
@@ -88,30 +93,35 @@ export DOCKER_TAG_DEBIAN11=avstephen/marte2-sigtools-debian11:${CODENAME_DEBIAN1
 
 build_debian11() {
 
-  TARGET=$1
-  do_cache=$2
+	TARGET=$1
+	invalidate_cache=$2
 
-  if [ x"$do_cache" == "xy" ]
-  then
-    CACHE_OPT="--build-arg CACHE_DATE=$(date +%s) "
-  fi
+	if [ x"$invalidate_cache" == "xy" ]
+	then
+		CACHE_OPT="--build-arg CACHE_DATE=$(date +%s) "
+	fi
 
-  this_log="build.debian11.${TARGET}.${CODENAME_DEBIAN11}.$(date +%s)"
+	this_log="build.debian11.${TARGET}.${CODENAME_DEBIAN11}.$(date +%s)"
 
-  time docker build "${CACHE_OPT:=' '}" \
-    --target "${TARGET}" \
-    -t "${DOCKER_TAG_DEBIAN11}" \
-    -f "${DOCKERFILE_DEBIAN11}" . 2>&1 | tee "$this_log"
+	time docker build "${CACHE_OPT:=' '}" \
+		--target "${TARGET}" \
+		-t "${DOCKER_TAG_DEBIAN11}" \
+		-f "${DOCKERFILE_DEBIAN11}" . 2>&1 | tee "$this_log"
 
-  ln -fs "$this_log" debian11.last.log
-    
+	ln -fs "$this_log" debian11.last.log
+
 }
 
 debian11() {
-build_debian11 m2st_base n
-build_debian11 m2st_packages n
-build_debian11 m2st_dependencies n
-build_debian11 m2st_built n
+	build_debian11 m2st_base n
+	build_debian11 m2st_packages n
+	build_debian11 m2st_dependencies n
+	build_debian11 m2st_built n
 }
 
 
+###
+### Main
+###
+
+ubuntu1804
